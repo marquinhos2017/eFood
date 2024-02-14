ecff228aa11ffcc58a78f025c5cc25f9bb221d7c
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-02-14 10:53:18 -0900

    Criando o modal estatico, ainda sem funcionalidade

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -1,77 +1,113 @@
 import { Item } from '../Item'
-import { Container, List } from './styles'
-
+import { Container, ContentInfo, List, Modal, ModalContent } from './styles'
+import close from '../../assets/fechar.png'
 export const ItemList = () => (
-  <Container>
-    <div className="container">
-      <List>
-        <Item
-          title={'Pizza Marguerita'}
-          description={
-            'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
-          }
-          infos={['Destaque da semana', 'Japonesa']}
-          image={
-            'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
-          }
-          id={1}
-        />
-        <Item
-          title={'Pizza Marguerita'}
-          description={
-            'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
-          }
-          infos={['Destaque da semana', 'Japonesa']}
-          image={
-            'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
-          }
-          id={2}
-        />
-        <Item
-          title={'Pizza Marguerita'}
-          description={
-            'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
-          }
-          infos={['Destaque da semana', 'Japonesa']}
-          image={
-            'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
-          }
-          id={3}
-        />
-        <Item
-          title={'Pizza Marguerita'}
-          description={
-            'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
-          }
-          infos={['Destaque da semana', 'Japonesa']}
-          image={
-            'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
-          }
-          id={4}
-        />
-        <Item
-          title={'Pizza Marguerita'}
-          description={
-            'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
-          }
-          infos={['Destaque da semana', 'Japonesa']}
-          image={
-            'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
-          }
-          id={5}
-        />
-        <Item
-          title={'Pizza Marguerita'}
-          description={
-            'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
-          }
-          infos={['Destaque da semana', 'Japonesa']}
-          image={
-            'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
-          }
-          id={6}
-        />
-      </List>
-    </div>
-  </Container>
+  <>
+    <Container>
+      <div className="container">
+        <List>
+          <Item
+            title={'Pizza Marguerita'}
+            description={
+              'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+            }
+            infos={['Destaque da semana', 'Japonesa']}
+            image={
+              'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
+            }
+            id={1}
+          />
+          <Item
+            title={'Pizza Marguerita'}
+            description={
+              'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+            }
+            infos={['Destaque da semana', 'Japonesa']}
+            image={
+              'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
+            }
+            id={2}
+          />
+          <Item
+            title={'Pizza Marguerita'}
+            description={
+              'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+            }
+            infos={['Destaque da semana', 'Japonesa']}
+            image={
+              'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
+            }
+            id={3}
+          />
+          <Item
+            title={'Pizza Marguerita'}
+            description={
+              'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+            }
+            infos={['Destaque da semana', 'Japonesa']}
+            image={
+              'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
+            }
+            id={4}
+          />
+          <Item
+            title={'Pizza Marguerita'}
+            description={
+              'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+            }
+            infos={['Destaque da semana', 'Japonesa']}
+            image={
+              'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
+            }
+            id={5}
+          />
+          <Item
+            title={'Pizza Marguerita'}
+            description={
+              'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+            }
+            infos={['Destaque da semana', 'Japonesa']}
+            image={
+              'https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg'
+            }
+            id={6}
+          />
+        </List>
+      </div>
+    </Container>
+    <Modal>
+      <ModalContent className="container">
+        <header>
+          <h4></h4>
+          <img src={close} />
+        </header>
+        <ContentInfo>
+          <img
+            src="https://img.grouponcdn.com/bynder/2nSnS4pXyy5Kx8uPtFYXFkrpW1PC/2n-2048x1229/v1/t600x362.jpg"
+            alt="SpiderMan"
+          ></img>
+          <div>
+            <h4>Pizza Marguerita</h4>
+            <p>
+              A pizza Margherita é uma pizza clássica da culinária italiana,
+              reconhecida por sua simplicidade e sabor inigualável. Ela é feita
+              com uma base de massa fina e crocante, coberta com molho de tomate
+              fresco, queijo mussarela de alta qualidade, manjericão fresco e
+              azeite de oliva extra-virgem. A combinação de sabores é perfeita,
+              com o molho de tomate suculento e ligeiramente ácido, o queijo
+              derretido e cremoso e as folhas de manjericão frescas, que
+              adicionam um toque de sabor herbáceo. É uma pizza simples, mas
+              deliciosa, que agrada a todos os paladares e é uma ótima opção
+              para qualquer ocasião.
+              <br />
+              <br />
+              Serve: de 2 a 3 pessoas
+            </p>
+            p
+          </div>
+        </ContentInfo>
+      </ModalContent>
+      <div className="overlay"></div>
+    </Modal>
+  </>
 )
--- src/components/ItemList/styles.ts
+++ src/components/ItemList/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
 
 export const Container = styled.section`
   padding: 50px;
@@ -9,4 +10,70 @@ export const List = styled.ul`
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 32px;
   row-gap: 32px;
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+
+export const ModalContent = styled.div`
+  padding: 32px;
+  position: relative;
+  z-index: 1;
+  max-width: 960px;
+  background-color: ${cores.vermelho};
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: bold;
+    }
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+`
+
+export const ContentInfo = styled.div`
+  display: flex;
+
+  img {
+    height: 200px;
+    width: 200px;
+    margin-right: 24px;
+  }
+
+  p {
+    color: ${cores.fundo};
+    font-size: 14px;
+  }
+
+  h4 {
+    font-size: 16px;
+    color: ${cores.fundo};
+    margin-bottom: 16px;
+  }
 `
